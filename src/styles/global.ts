f10cd90e93645e0a11e6c6d93e8a5960c50951f9
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-09-02 16:49:29 -0300

    Retuning to home now

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        background-color: #333;
+        background-color: ${props => props.theme.primary};
         color: #fff;
     }
 
