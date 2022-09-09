bee9e98555221d7b0dc1fa1e3027fbb757a8ea1d
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-09-09 20:43:53 -0300

    03 - Header & Layout Rocketseat

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
     body{
         background-color: ${(props) => props.theme.gray};
-        color: #black;
+        color: white;
     }
 
     body, input, textarea, button {
